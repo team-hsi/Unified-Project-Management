37e4d21e924a67e550fb8111f05c129f16e07c55
HikmaAnwar <hikmaanwar89@gmail.com>
2025-03-20 00:41:49 +0300

    feat(project): simplify Page component by removing BoardCover and TaskList

--- web/app/projects/[id]/page.tsx
+++ web/app/projects/[id]/page.tsx
@@ -1,13 +1,5 @@
-import BoardCover from "@/components/project/project-boardcover";
-import TaskList from "@/components/project/list-content";
-
 const Page = () => {
-  return (
-    <div className="bg-muted/50 flex-1 rounded-xl">
-      <BoardCover />
-      <TaskList />
-    </div>
-  );
+  return <div className="bg-muted/50 flex-1 rounded-xl" />;
 };
 
 export default Page;
