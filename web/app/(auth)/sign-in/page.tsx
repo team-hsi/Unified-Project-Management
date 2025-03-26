246dd3ce5cbcdacf0d17b4d3c66c184faead4f27
Hunde-D <hunde.ddh@gmail.com>
2025-03-26 21:40:55 +0300

    feat: auth with out validation

--- web/app/(auth)/sign-in/page.tsx
+++ web/app/(auth)/sign-in/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/components/auth/login-form";
 
 const page = () => {
   return (
-    <div className="mx-auto flex h-screen items-center justify-center">
+    <div className="mx-auto flex h-screen items-center justify-center px-4 sm:px-6 lg:px-8">
       <LoginForm />
     </div>
   );
