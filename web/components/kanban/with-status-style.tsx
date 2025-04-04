1fcd6b13a635c9ff241ba6556fe139dc4e7af898
Hunde-D <hunde.ddh@gmail.com>
2025-04-04 23:10:38 +0300

    refactor: remove unused components and dependencies

--- web/components/kanban/with-status-style.tsx
+++
@@ -1,8 +0,0 @@
-// export const WithStatusStyle = (Component: React.ComponentType) => {
-//   const WithStatusStyles = (props: Omit<P, keyof WithStatusStylesProps> & StatusIdProps) => {
-//     const { statusId, ...rest } = props
-//     const statusStyles = getStatusStyles(statusId)
-
-//     // Cast is needed due to TypeScript limitations with HOCs
-//     return <Component {...(rest as any)} statusStyles={statusStyles} />
-//   }
