f6e669894fdb7c66ac39a0254f3faefd96b3b4d1
Hunde-D <hunde.ddh@gmail.com>
2025-05-05 16:22:46 +0300

    refactor(select-space): simplify SelectWorkspace component and integrate userWorkspaces hook

--- web/app/(auth)/select-workspace/page.tsx
+++ web/app/(auth)/select-workspace/page.tsx
@@ -1,11 +1,22 @@
 import { getUserWorkspaces } from "@/actions/api/workspace/queries";
+import { getSession } from "@/actions/core/dal";
 import SelectWorkspace from "@/feature/shared/components/workspace/select-space";
+import { getQueryClient } from "@/lib/query-client/get-query-client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 const page = async () => {
-  const workspaces = await getUserWorkspaces();
-  console.log("workspaces-page", workspaces);
+  const session = await getSession();
+  const queryClient = getQueryClient();
 
-  return <SelectWorkspace workspaces={workspaces} />;
+  queryClient.prefetchQuery({
+    queryKey: [session?.userId, "workspaces"],
+    queryFn: getUserWorkspaces,
+  });
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <SelectWorkspace />;
+    </HydrationBoundary>
+  );
 };
 
 export default page;
