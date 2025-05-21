655c3c93fa2c2b69f3cdedfb51c028f379bb2e4b
Hunde-D <hunde.ddh@gmail.com>
2025-05-21 17:34:51 +0300

    feat: doc page metadata

--- web/app/(protected)/[workspaceId]/[projectId]/[docId]/page.tsx
+++ web/app/(protected)/[workspaceId]/[projectId]/[docId]/page.tsx
@@ -2,11 +2,26 @@ import { getDocumentById } from "@/actions/api/document/queries";
 import Editor from "@/feature/documentation/editor";
 import { getQueryClient } from "@/lib/query-client/get-query-client";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Metadata } from "next";
 import React from "react";
 type Props = {
   params: Promise<{ docId: string; projectId: string }>;
-  children: React.ReactNode;
 };
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { docId } = await params;
+
+  const document = await getDocumentById({ id: docId });
+  if (!document) {
+    return {
+      title: "Project Not Found",
+    };
+  }
+
+  return {
+    title: document.name || docId,
+  };
+}
 const page = async (props: Props) => {
   const params = await props.params;
   const queryClient = getQueryClient();
